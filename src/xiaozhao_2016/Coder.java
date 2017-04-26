4f771b582f517085ffdebe720ad53f90553b7d47
mindCold <lhtancn@qq.com>
2017-04-26 17:24:29 +0800

    寻找Coder@2016xiaozhao@nowcoder@V1.1

--- src/xiaozhao_2016/Coder.java
+++ src/xiaozhao_2016/Coder.java
@@ -12,7 +12,6 @@ public class Coder {
             return null;
         }
         List<StrInfo> list = new ArrayList<>();
-
         for(int i = 0; i < A.length; ++i) {
             String str = A[i].toLowerCase();
             int count = findCount(str);
