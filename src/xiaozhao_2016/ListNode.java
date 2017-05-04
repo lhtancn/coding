944823bf0867f0bae53a8a3a4afb046b5cacad0c
mindCold <lhtancn@qq.com>
2017-05-04 21:03:32 +0800

    辅助节点类@2016xiaozhao@nowcoder@V1.0

---
+++ src/xiaozhao_2016/ListNode.java
@@ -0,0 +1,14 @@
+package xiaozhao_2016;
+
+/**
+ * Created by lhtan on 5/4/2017.
+ */
+
+public class ListNode {
+    public int val;
+    public ListNode next = null;
+
+    public ListNode(int val) {
+        this.val = val;
+    }
+}
